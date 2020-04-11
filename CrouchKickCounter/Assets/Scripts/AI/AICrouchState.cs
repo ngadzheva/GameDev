a10614e8e0c73a1d34bfac95879bf835bff7bdc5
Nevena Gadzheva <nmgadzheva@gmail.com>
2020-04-11 22:47:03 +0300

    Add Crouch Kick Counter task

---
+++ CrouchKickCounter/Assets/Scripts/AI/AICrouchState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICrouchState : StateMachineBehaviour {
+
+    private MovementController movementController;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        movementController = animator.GetComponent<MovementController>();
+        movementController.SetHorizontalMoveDirection(0);
+
+        Transform player = GameObject.FindWithTag("Player").transform;
+        float direction = player.position.x - animator.transform.position.x;
+        movementController.TurnTowards(direction);
+
+        animator.SetTrigger("ShouldCrouchKick");
+    }
+}
