1dd687bc009709412d70f95fdfc80de7f23b1919
Nevena Gadzheva <nmgadzheva@gmail.com>
2020-04-25 18:28:36 +0300

    Add Pixelization Shader task

---
+++ Pixelization Shader/Assets/High Poly Chess Pack/Scripts/Pixelize.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Pixelize : MonoBehaviour {
+    public Material material;
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest) {
+        Graphics.Blit(src, dest, material);
+    }
+}
